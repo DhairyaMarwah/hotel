732f2342d96731aa8b7677d3034a3a27ef949578
dhairyamarwah <dhairyamarwah01@gmail.com>
2022-12-29 14:58:50 +0530

    update

--- src/components/HotelCards.js
+++ src/components/HotelCards.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react";
 import { useEffect, useState } from "react";
 import Like from "../assets/like.svg";
-import moment from 'moment';
+import moment from "moment";
 import Location from "../assets/location.svg";
 const HotelCards = ({
   value,
@@ -20,23 +20,27 @@ const HotelCards = ({
       "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
       "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
     },
+  }; 
+  const fetchData = async () => {
+    try {
+      const res = await fetch(`https://booking-com.p.rapidapi.com/v1/hotels/search?room_number=1&checkin_date=${moment(value[0]).format("YYYY-MM-DD")}&checkout_date=${moment(value[1]).format("YYYY-MM-DD")}&units=metric&order_by=${orderByValue}&adults_number=${number.adults}&filter_by_currency=AED&locale=${selectedOption}&dest_id=-553173&dest_type=city&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&children_ages=5%2C0&children_number=${number.children}&page_number=0&include_adjacency=true`,options)
+      var json = await res.json();
+      setData(json.result);
+    } catch (err) {
+      console.log(err);
+    }
   };
-  // console.log(data2);
-    const fetchData = async () => {
-        try {
-    const res = await fetch(`https://booking-com.p.rapidapi.com/v1/hotels/search?room_number=1&checkin_date=${moment(value[0]).format("YYYY-MM-DD")}&checkout_date=${moment(value[1]).format("YYYY-MM-DD")}&units=metric&order_by=${orderByValue}&adults_number=${number.adults}&filter_by_currency=AED&locale=${selectedOption}&dest_id=-553173&dest_type=city&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&children_ages=5%2C0&children_number=${number.children}&page_number=0&include_adjacency=true`,options)
-          var json = await res.json(); 
-          setData(json.result); 
-        } catch (err) {
-          console.log(err);
-        }
-      };
-      useEffect(() => {
-        fetchData();
-      }, [orderByValue,value,selectedOption,number]);
+  useEffect(() => {
+    fetchData();
+  }, [orderByValue, value, selectedOption, number]);
   return (
-    <div ref={scollToRef}   className="all-hotels">
-        {data?.slice(0, 15)?.filter((hotel) => hotel?.hotel_name.toLowerCase().includes(searchTerm.toLowerCase())).map((hotel, index) => (
+    <div ref={scollToRef} className="all-hotels">
+      {data
+        ?.slice(0, 15)
+        ?.filter((hotel) =>
+          hotel?.hotel_name.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+        .map((hotel, index) => (
           <div className="hotel-card">
             <div className="hotel-card-img">
               <img src={data2[index]?.images} alt="" />
@@ -71,8 +75,8 @@ const HotelCards = ({
             </div>
           </div>
         ))}
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default HotelCards
+export default HotelCards;
